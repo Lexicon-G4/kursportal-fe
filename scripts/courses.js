90afaddca0ece75a8152cc68720756332e67bc4b
Alexander Stauch <blacke4dawn@gmail.com>
2026-08-04 12:56:49 +0200

    adding course loading operations

--- scripts/courses.js
+++ scripts/courses.js
@@ -1,6 +1,8 @@
 import { ServerGet } from "./api";
 
 const basePath = "kurser";
+const listElementID = "kurslista";
+const detailsElementID = "kursfält";
 
 export async function FetchAllCourses() {
   try {
@@ -16,6 +18,44 @@ export async function FetchCourse(id) {
     return ServerGet(`${basePath}/${id}`);
   } catch (error) {
     console.error(`Failed to fetch course ${id}:`, error);
-    return [];
+    return null;
   }
+}
+
+export function LoadAllCourses() {
+  const listElement = document.querySelector(`#${listElementID}`);
+  const courses = await FetchAllCourses();
+
+  if (!courses) return;
+
+  listElement.replaceChildren();
+
+  courses.array.forEach(course => {
+    const linkNode = document.createElement("a");
+    linkNode.href ="#";
+    linkNode.dataset["course-id"] = course.id;
+    linkNode.innerText = course.title;
+
+    const listNode = document.createElement("li");
+    listNode.appendChild(listNode);
+  });
+}
+
+export function LoadCourseDetails(id) {
+  const detailsElement = document.querySelector(`#${detailsElementID}`);
+  const courseData = FetchCourse(id);
+
+  if (!courseData) return;
+
+  detailsElement.replaceChildren();
+
+  detailsElement.innerHTML = `<div id="kursdetaljer">
+    <h2 class="kurstitel">${course.title}</h2>
+    <div class="kursbeskrivning">
+      <p>${course.desription}</p>
+    </div>
+    <div class="kurslärare">${course.teacher}</div>
+    <div class="kurstekniker"></div>
+    <div class="kurstaggar"></div>
+  </div>`
 }
